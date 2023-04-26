876d04c7f267f5a19e3af41fd18a046d607ebe06
Serizao <11671895+Serizao@users.noreply.github.com>
2023-04-26 10:46:07 +0200

    fix issue #152
    add greedy properties to link regex

--- backend/src/lib/html2ooxml.js
+++ backend/src/lib/html2ooxml.js
@@ -251,7 +251,7 @@ function html2ooxml(html, style = "") {
   let dataXml = xml(filteredXml);
   dataXml = dataXml.replace(/w:numId w:val="{2-0}"/g, 'w:numId w:val="2"'); // Replace numbering to have correct value
   //a little dirty but until we do better it works
-  dataXml = dataXml.replace(/\{_\|link\|_\{(.*)\|\-\|(.*)\}_\|link\|_\}/gm, '<w:r><w:fldChar w:fldCharType="begin"/></w:r><w:r><w:instrText xml:space="preserve"> HYPERLINK $2 </w:instrText></w:r><w:r><w:fldChar w:fldCharType="separate"/></w:r><w:r><w:rPr><w:rStyle w:val="Hyperlink"/></w:rPr><w:t> $1 </w:t> </w:r><w:r><w:fldChar w:fldCharType="end"/></w:r>')
+  dataXml = dataXml.replace(/\{_\|link\|_\{(.*?)\|\-\|(.*?)\}_\|link\|_\}/gm, '<w:r><w:fldChar w:fldCharType="begin"/></w:r><w:r><w:instrText xml:space="preserve"> HYPERLINK $2 </w:instrText></w:r><w:r><w:fldChar w:fldCharType="separate"/></w:r><w:r><w:rPr><w:rStyle w:val="Hyperlink"/></w:rPr><w:t> $1 </w:t> </w:r><w:r><w:fldChar w:fldCharType="end"/></w:r>')
   console.log(dataXml)
   return dataXml;
 }
