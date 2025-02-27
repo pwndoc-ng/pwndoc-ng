e46c6b365122556abeea333687a2849748f83c88
Serizao <11671895+Serizao@users.noreply.github.com>
2025-02-27 16:11:00 +0100

    Update report-generator.js

--- backend/src/lib/report-generator.js
+++ backend/src/lib/report-generator.js
@@ -1035,7 +1035,8 @@ async function prepAuditData(data, settings) {
             affected: finding.scope || "",
             status: finding.status || "",
             category: $t(finding.category) || $t("No Category"),
-            identifier: "IDX-" + utils.lPad(finding.identifier)
+            identifier: "IDX-" + utils.lPad(finding.identifier),
+            unique_id: finding._id
         }
         // Remediation Complexity color 
         if (tmpFinding.remediationComplexity === 1) tmpFinding.remediation.cellColorComplexity = cellLowColorRemediationComplexity
